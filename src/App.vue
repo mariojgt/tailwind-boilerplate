2243c4a99e9bdea0526216cf8950a293cc62ad39
mario tarosso <mariojgt2@gmail.com>
2022-02-06 00:51:30 +0000

    up

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script setup >
+import HelloWorld from "./pages/HelloWorld.vue";
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/Components/Grid1.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="card shadow-lg compact side bg-base-100">
+        <div class="flex-row items-center space-x-4 card-body">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup></script>
